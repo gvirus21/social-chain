5291d1663a59f598a156ca7ed92501513e65e262
gvirus21 <gouravkumar21.dev@gmail.com>
2023-02-05 20:18:40 +0530

    WIP - profile page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { CgProfile } from "react-icons/cg";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
+    <main className="flex h-screen  w-screen">
+      <section className="flex m-auto w-4/6 md:px-20 h-2/6 mt-10">
+        <div className="flex w-2/6">
+          <CgProfile size={"full"} color="pink" />
+        </div>
+        <div className="ml-16 pt-12">
+          <h1 className="text-4xl font-semibold">Full Name</h1>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
